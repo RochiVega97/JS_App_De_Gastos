6b5f35f5a3dbbb893de6b43c60d40b1acbbb75d9
Rocio <rochimarivega@hotmail.com>
2022-07-24 18:23:30 -0300

    version 0

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JS_App_De_Gastos
+APP para dividir gastos.
+Autor: Vega Rocio
---
+++ VegaRocio.js
@@ -0,0 +1,39 @@
+const arr = [];
+const List = document.getElementById("listResult")
+const Result = document.getElementById("comments")
+let newComment = document.createElement("p")
+
+function tomoValores(){
+    let names = document.getElementById("nombre").value
+    let value = document.getElementById("gasto").value
+    let descripcion_gasto = document.getElementById("desc").value
+
+    arr.push([names, parseFloat(value),descripcion_gasto])
+}
+function printList() {
+    let newList = document.createElement("li")
+    for (i of arr) {
+        newList.innerHTML = `<li class="list-group-item bg-primary bg-gradient fw-bold text-white" style="background-color: blue">${i[0]}: \$${i[1]} \ ${i[2]}</li>` 
+        List.appendChild(newList)
+    }
+}
+function printComment() {
+    newComment.innerHTML = '<p style="background-color: #D3DEDC; padding-left:16px;">Total: $' + suma + '<br> A cada uno le toca: $' + Valores +'</p>'
+    List.appendChild(newComment)
+}
+function splitBills(Montos) {
+    suma = 0;
+        for (let Valor of Montos){
+            suma = suma + Valor[1]  ;
+        }
+        Valores = suma / (arr.length);
+        return Valores, suma
+}
+
+function sumbit() {
+    tomoValores()
+    splitBills(arr)
+    printList()
+    console.log(suma)
+    printComment()
+}
