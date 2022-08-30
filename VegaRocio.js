7effd2c7b5b8c45c271b1519591f5d3ca6dc9641
RochiVega97 <100883429+RochiVega97@users.noreply.github.com>
2022-08-30 20:31:22 -0300

    .

--- VegaRocio.js
+++ VegaRocio.js
@@ -75,8 +75,6 @@ botonMostrarGastos.addEventListener('click',()=>{
             Toastify({
                 text: "Se ha eliminado el gasto",
                 duration: 3000,
-                destination: "https://github.com/apvarun/toastify-js",
-                newWindow: true,
                 close: true,
                 gravity: "top", // `top` or `bottom`
                 position: "left", // `left`, `center` or `right`
