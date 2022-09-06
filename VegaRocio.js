3a4685c7ed5a1b0e595d672c50927691d224d1c4
RochiVega97 <100883429+RochiVega97@users.noreply.github.com>
2022-09-06 18:43:08 -0300

    .

--- VegaRocio.js
+++ VegaRocio.js
@@ -137,18 +137,43 @@ function printComment() {
     List.appendChild(newComment)
 }
 
-const divDolar=document.getElementById('divDolar')
-fetch('https://criptoya.com/api/dolar')
-.then(response => response.json())
-.then(({solidario, ccl, ccb, blue}) =>{
-    divDolar.innerHTML=`
-        <p>Solidario: $${solidario}</p>
-        <p>CCL: $${ccl}</p>
-        <p>CCB: $${ccb}</p>
-        <p>Blue: $${blue}</p>
-    `
-})
+const template = document.getElementById('template').content
+const cards=document.getElementById('cards-dolar')
+const fragment= document.createDocumentFragment()
+document.addEventListener("DOMContentLoaded", () => fetchData());
+const fetchData = async () => {
+  try {
+    const res = await fetch(
+      "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
+    );
+    const data = await res.json();
+    console.log(template)
+    pintarCards(data)
+  } catch (error) {
+    console.log(error);
+  }
+};
+const pintarCards = data => {
+  const fecha = new Date()
+  let variacion
+  data.forEach(item => {
+    if(item.casa.variacion==undefined){
+      variacion='no disponible'
+    }
+    else{
+      variacion=item.casa.variacion
+    }
+    template.querySelector('.card-title').textContent = item.casa.nombre
+    template.querySelector('.compra').textContent = item.casa.compra
+    template.querySelector('.venta').textContent = item.casa.venta
+    template.querySelector('.card-subtitle').textContent = "variacion: " + variacion
+    template.querySelector('.fecha').textContent = fecha.getDate()+'/'+(fecha.getMonth()+1)+'/'+fecha.getFullYear()+' '+fecha.getHours()+':'+fecha.getMinutes()
+    const clone = template.cloneNode(true)
+    fragment.appendChild(clone)
+  });
 
+  cards.appendChild(fragment)
+}
 
 
 
